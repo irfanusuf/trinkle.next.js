7434569a796ec81871abbc3cc19b64d306fc3933
Irfan Yousuf <irfanusuf33@gmail.com>
2025-12-30 15:28:58 +0530

    addedGTM

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ import { ThemeProvider } from "@/components/theme/ThemeProvider";
 import ThemeToggle from "@/components/theme/ThemeToggle";
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
+import Script from "next/script";
+
+
 // import { createContext } from "react";
 
 const geistSans = Geist({
@@ -81,14 +84,33 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
 
 
-         <ToastContainer />
+
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+        >
+          {`
+    (function(w,d,s,l,i){w[l]=w[l]||[];
+    w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+    var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+    j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;
+    f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-W8PLFNWR');
+  `}
+        </Script>
+
+
+        <ToastContainer />
 
         <ThemeProvider>
+
+
           <Store>
             {children}
           </Store>
